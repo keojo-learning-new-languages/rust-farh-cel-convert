ec5a4294374a20da12417f213d472ca4db371592
jocelin <queau.j@gmail.com>
2024-05-12 17:21:57 +0200

    adding ratatui and quit commands

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::io::{self, stdout};
+
+use crossterm::{
+    event::{self, Event, KeyCode, KeyModifiers},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::*, widgets::*};
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn ui(frame: &mut Frame) {
+    frame.render_widget(
+        Paragraph::new("Bibop program that convert fahrenheit to celsius or celsius to fahrenheit"),
+        frame.size(),
+    );
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press
+                && key.code == KeyCode::Char('c')
+                && key.modifiers == KeyModifiers::CONTROL
+            {
+                return Ok(true);
+            }
+            if key.kind == event::KeyEventKind::Press
+                && key.code == KeyCode::Char('q')
+                && key.modifiers == KeyModifiers::CONTROL
+            {
+                return Ok(true);
+            }
+
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
